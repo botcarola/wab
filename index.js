cbe993a6e75839810b8ad092ec160fa82282ad11
botcarola <celerner4@gmail.com>
2023-06-04 19:45:07 -0300

    prueba 4

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 document.querySelector("button").onclick = () => {
-    navigator.bluetooth.requestDevice({
-        acceptAllDevices: true,
-        optionalServices: ['battery_service'] // Required to access service later.
-      })
-      .then( device => alert(`Su porcentaje es: ${device}`))
-      .catch(error => { console.error(error); });
+    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+        .then(device => device.gatt.connect())
+        .then(server => {
+            // Getting Battery Service…
+            return server.getPrimaryService('battery_service');
+        })
+        .then(service => {
+            // Getting Battery Level Characteristic…
+            return service.getCharacteristic('battery_level');
+        })
+        .then(characteristic => {
+            // Reading Battery Level…
+            return characteristic.readValue();
+        })
+        .then(value => {
+            alert(`Battery percentage is ${value.getUint8(0)}`);
+        })
+        .catch(error => { alert(error); });
 }
